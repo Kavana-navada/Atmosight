0400823410d8d6fda372dfcc764de99c697d45b5
kavana <kavananavada03@gmail.com>
2025-06-22 19:34:47 +0530

    added toggle button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useTheme } from "@/context/theme-provider";
+import { Sun, Moon } from 'lucide-react';
 
 const Header = () => {
-  return (
+
+    const {theme,setTheme}=useTheme();
+    const isDark=theme==="dark";
+    return (
     <header className=' sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur py-2  supports-[backdrop-filter]:bg-background/60'>
         <div className='container mx-auto flex h-12 items-center justify-between px-4'>
             <Link to ={"/"}>
-                <img src="/lg.png" alt="Logo" className="h-8 w-8  inline-block " />
-                <span className="text-xl font-bold ml-2">Atmosight</span>
+                <img src="/logo.png" alt="Logo" className="h-8 w-8  inline-block " />
+                <span className="text-xl font-bold ml-2 text-blue-700" >Atmosight</span>
             </Link>
 
             <div>
-                
+                {/*search*/}
+                <button
+                    onClick={() => setTheme(isDark ? "light" : "dark")}
+                    className={`flex items-center cursor-pointer transition-transform duration-500 ${isDark ? 'rotate-180' : 'rotate-0'}`}
+                    aria-label={`Switch to ${isDark ? 'light' : 'dark'} mode`}
+                    type="button"
+                >
+                    {isDark ? (
+                        <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
+                    ) : (
+                        <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
+                    )}
+                </button>
             </div>
         </div>
       
